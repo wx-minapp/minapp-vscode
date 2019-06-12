81bf9e0710c48dd4656cf736ea2cebadab529401
NewFuture <New.Future@microsoft.com>
2019-06-13 02:36:36 +0800

    feat(style): support all nested classname #33

--- src/plugin/lib/StyleFile.ts
+++ src/plugin/lib/StyleFile.ts
@@ -4,7 +4,6 @@ import { TextDocument, window, Position } from 'vscode'
 import { quickParseStyle } from './quickParseStle'
 import { Config } from './config'
 import { getRoot } from './helper'
-import loadScss from './loadScss'
 
 export interface Style {
   name: string
@@ -19,17 +18,13 @@ export interface StyleFile {
 
 const fileCache: { [file: string]: { mtime: Date, value: StyleFile } } = {}
 
-function isScss(file: string): boolean {
-  return /\.s[ac]ss/.test(file)
-}
-
 export function parseStyleFile(file: string) {
   try {
     let cache = fileCache[file]
     let editor = window.visibleTextEditors.find(e => e.document.fileName === file)
     if (editor) {
       let content = editor.document.getText()
-      return { file, styles: quickParseStyle(isScss(file) ? loadScss({ data: content, file }) : content) }
+      return { file, styles: quickParseStyle(content) }
     } else {
       const stat = fs.statSync(file)
       if (cache && stat.mtime <= cache.mtime) {
@@ -39,7 +34,7 @@ export function parseStyleFile(file: string) {
         mtime: stat.mtime,
         value: {
           file,
-          styles: quickParseStyle(isScss(file) ? loadScss({ file }) : fs.readFileSync(file).toString())
+          styles: quickParseStyle(fs.readFileSync(file).toString())
         }
       }
       fileCache[file] = cache
--- src/plugin/lib/quickParseStle.ts
+++ src/plugin/lib/quickParseStle.ts
@@ -1,13 +1,10 @@
-import { match, getPositionFromIndex } from './helper'
+import { match } from './helper'
 import { Position } from 'vscode'
+import { Plugin, } from 'postcss'
+/// @ts-ignore
+import * as nested from 'postcss-nested'
 
 const styleRegexp = /\.[a-zA-Z][\w-\d_]*/g
-const styleWithDocRegexp = /\/\*([\s\S]*?)\*\/[\s\r\n]*[^\.\{\}]*\.([a-zA-Z][\w-\d_]*)/g
-
-const styleSingleCommentRegexp = /\/\/.*/g
-const styleMultipleCommentRegExp = /\/\*[\s\S]*?\*\//g
-
-const startStarRegexp = /^\s*\*+ ?/mg
 
 export namespace quickParseStyle {
   export interface Options {
@@ -21,36 +18,31 @@ export namespace quickParseStyle {
  *
  * 样式文件可能是 scss/less/css 所以不需要解析成 ast，只需要用正则即可
  */
-export function quickParseStyle(styleContent: string, {unique}: quickParseStyle.Options = {}) {
-  let style: Array<{doc: string, pos: Position, name: string}> = []
-  let content = styleContent
-    .replace(styleSingleCommentRegexp, replacer)            // 去除单行注释
-    .replace(styleMultipleCommentRegExp, replacer)          // 去除多行注释
+export function quickParseStyle(styleContent: string, { unique }: quickParseStyle.Options = {}) {
+  let style: Array<{ doc: string, pos: Position, name: string }> = []
+  const result = (nested as Plugin<any>).process(styleContent)
 
-  match(content, styleRegexp).forEach(mat => {
-    const name = mat[0].substr(1)
-    if (!unique || !style.find(s => s.name === name)) {
-      style.push({doc: '', pos: getPositionFromIndex(content, mat.index), name})
+  result.root.walkRules((rule) => {
+    if (rule.type === 'rule') { // 只遍历正常rule
+      // 起始位置
+      let ruleStart = { line: 1, column: 1 }
+      if (rule.source && rule.source.start) {
+        ruleStart = rule.source.start
+      }
+      const endLine = rule.source && rule.source.end ? rule.source.end.line : ruleStart.line
+      // 文档提示
+      const doc = styleContent.split(/\n/g).splice(ruleStart.line - 1, endLine - ruleStart.line + 1).join('')
+      rule.selectors.forEach(selector => {
+        match(selector, styleRegexp).forEach(mat => {
+          // 提取所有class
+          const name = mat[0].substr(1)
+          if (!unique || !style.find(s => s.name === name)) {
+            style.push({ doc, name, pos: new Position(ruleStart.line, ruleStart.column) })
+          }
+        })
+      })
     }
   })
 
-
-  // 再来获取带文档的 className
-  styleContent.replace(styleWithDocRegexp, (raw, doc, name) => {
-    style.forEach(s => {
-      if (s.name === name) s.doc = parseDoc(doc)
-      return s.name === name
-    })
-    return ''
-  })
-
   return style
-}
-
-function replacer(raw: string) {
-  return ' '.repeat(raw.length)
-}
-
-function parseDoc(doc: string) {
-  return doc.replace(startStarRegexp, '').trim()
 }
