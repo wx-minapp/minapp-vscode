9a6688a5696d7b5596557b05cc54526cc7bf9433
NewFuture <New.Future@microsoft.com>
2019-06-15 13:30:30 +0800

    fix(style): rollback when parse failed

--- src/plugin/lib/quickParseStle.ts
+++ src/plugin/lib/quickParseStle.ts
@@ -1,10 +1,16 @@
-import { match } from './helper'
+import { match, getPositionFromIndex } from './helper'
 import { Position } from 'vscode'
 import { Plugin, } from 'postcss'
 /// @ts-ignore
 import * as nested from 'postcss-nested'
 
 const styleRegexp = /\.[a-zA-Z][\w-\d_]*/g
+const styleWithDocRegexp = /\/\*([\s\S]*?)\*\/[\s\r\n]*[^\.\{\}]*\.([a-zA-Z][\w-\d_]*)/g
+
+const styleSingleCommentRegexp = /\/\/.*/g
+const styleMultipleCommentRegExp = /\/\*[\s\S]*?\*\//g
+
+const startStarRegexp = /^\s*\*+ ?/mg
 
 export namespace quickParseStyle {
   export interface Options {
@@ -15,11 +21,46 @@ export namespace quickParseStyle {
 
 /**
  * 解析样式文件内容成 className 和 doc 的形式
- *
- * 样式文件可能是 scss/less/css 所以不需要解析成 ast，只需要用正则即可
+ * - css/scss 生成 nested tree 解析
+ * - less stylus 用正则
  */
-export function quickParseStyle(styleContent: string, { unique }: quickParseStyle.Options = {}) {
-  let style: Array<{ doc: string, pos: Position, name: string }> = []
+export function quickParseStyle(styleContent: string, {unique}: quickParseStyle.Options = {}) {
+  try {
+    return parseNestedStyle(styleContent, {unique})
+  } catch (error) {
+    console.warn(error)
+  }
+  let style: Array<{doc: string, pos: Position, name: string}> = []
+  let content = styleContent
+    .replace(styleSingleCommentRegexp, replacer)            // 去除单行注释
+    .replace(styleMultipleCommentRegExp, replacer)          // 去除多行注释
+
+  match(content, styleRegexp).forEach(mat => {
+    const name = mat[0].substr(1)
+    if (!unique || !style.find(s => s.name === name)) {
+      style.push({doc: '', pos: getPositionFromIndex(content, mat.index), name})
+    }
+  })
+
+
+  // 再来获取带文档的 className
+  styleContent.replace(styleWithDocRegexp, (raw, doc, name) => {
+    style.forEach(s => {
+      if (s.name === name) s.doc = parseDoc(doc)
+      return s.name === name
+    })
+    return ''
+  })
+
+  return style
+}
+
+
+/**
+ * 解析嵌套格式
+ */
+function parseNestedStyle(styleContent: string, { unique }: quickParseStyle.Options = {}) {
+  const style: Array<{ doc: string, pos: Position, name: string }> = []
   const result = (nested as Plugin<any>).process(styleContent)
 
   result.root.walkRules((rule) => {
@@ -31,7 +72,7 @@ export function quickParseStyle(styleContent: string, { unique }: quickParseStyl
       }
       const endLine = rule.source && rule.source.end ? rule.source.end.line : ruleStart.line
       // 文档提示
-      const doc = styleContent.split(/\n/g).splice(ruleStart.line - 1, endLine - ruleStart.line + 1).join('')
+      const doc = styleContent.split(/\n/g).splice(ruleStart.line - 1, endLine - ruleStart.line + 1).join('\n')
       rule.selectors.forEach(selector => {
         match(selector, styleRegexp).forEach(mat => {
           // 提取所有class
@@ -45,4 +86,12 @@ export function quickParseStyle(styleContent: string, { unique }: quickParseStyl
   })
 
   return style
+}
+
+function replacer(raw: string) {
+  return ' '.repeat(raw.length)
+}
+
+function parseDoc(doc: string) {
+  return doc.replace(startStarRegexp, '').trim()
 }
