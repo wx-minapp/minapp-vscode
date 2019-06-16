9f32b233d5697ab45676297e9fd6aac10dc3f652
NewFuture <New.Future@microsoft.com>
2019-06-16 21:51:55 +0800

    fix(wxml): 标签结束位置判断

--- src/plugin/getTagAtPosition/getWxmlTag.ts
+++ src/plugin/getTagAtPosition/getWxmlTag.ts
@@ -3,10 +3,35 @@ MIT License http://www.opensource.org/licenses/mit-license.php
 Author Mora <qiuzhongleiabc^126.com> (https://github.com/qiu8310)
 *******************************************************************/
 
-import {TextDocument, Position, Range} from 'vscode'
+import {TextDocument, Position} from 'vscode'
 import {Tag, getAttrs, getAttrName} from './base'
 
 const TAG_REGEXP = /<([\w-:.]+)(\s+[^<>]*)?/g
+
+/**
+ * 获取标签的起始位置
+ * @param text
+ * @param pos
+ */
+function getBracketRange(text: string, pos: number): [number, number] {
+  const startBracket = Math.max(text.substr(0, pos).lastIndexOf('<'), 0)
+
+  let endBracket = text.indexOf('>', pos + 1)
+  if (endBracket < 0) {
+    // 未找到闭合 > 文件结束位置为结束
+    // 如 <image ... | EOF
+    endBracket = text.length
+  }
+
+  // 可能尚未输入闭合标签，取下一个标签的头<
+  // 此时找到的闭合标签是下一个标签
+  // <view xxx | ... <view ></view>
+  const nextStart = text.indexOf('<', pos + 1)
+  if (nextStart > 0 && nextStart < endBracket) {
+    endBracket = nextStart
+  }
+  return [startBracket, endBracket - startBracket]
+}
 
 /**
  * 提取标签 允许跨行
@@ -16,7 +41,7 @@ const TAG_REGEXP = /<([\w-:.]+)(\s+[^<>]*)?/g
 export function getWxmlTag(doc: TextDocument, pos: Position): null | Tag {
   let tag: null | Tag = null
   let offset = doc.offsetAt(pos)
-  let text = doc.getText(new Range(new Position(0, 0), new Position(pos.line + 1, 0)))
+  let text = doc.getText()
   // let line = doc.lineAt(pos.line).text
   let replacer = (char: string) => (raw: string) => char.repeat(raw.length)
 
@@ -27,13 +52,12 @@ export function getWxmlTag(doc: TextDocument, pos: Position): null | Tag {
   let attrFlagText = pureText.replace(/("[^"]*"|'[^']*')/g, replacer('%')) // 将引号中的内容也替换了
 
   // 标签起始位置
-  const lastBracket = attrFlagText.substr(0, offset).lastIndexOf('<', offset)
-  if (lastBracket >= 0) {
-    offset = offset - lastBracket
-    text = text.substr(lastBracket)
-    pureText = pureText.substr(lastBracket)
-    attrFlagText = attrFlagText.substr(lastBracket)
-  }
+  const [start, end] = getBracketRange(attrFlagText, offset)
+
+  offset = offset - start
+  text = text.substr(start, end)
+  pureText = pureText.substr(start, end)
+  attrFlagText = attrFlagText.substr(start, end)
 
   // console.log(pureLine)
   pureText.replace(TAG_REGEXP, (raw: string, name: string, attrstr: string, index: number) => {
