71c13101d2a478037474f172af45ab377752498c
NewFuture <New.Future@microsoft.com>
2019-06-22 16:12:51 +0800

    feat(propDefinition): 函数匹配支持参数表和异步函数

--- src/plugin/lib/ScriptFile.ts
+++ src/plugin/lib/ScriptFile.ts
@@ -8,8 +8,10 @@ function parseScriptFile(file: string, type: string, prop: string) {
   let locs: Location[] = []
 
   let reg: RegExp | null = null
-  let s = '\\s*'
-  let b = `\\(${s}\\)`
+  /**
+   * 空白符正则
+   */
+  const s = '\\s*'
 
   if (type === 'prop') {
     reg = new RegExp(`^${s}` + prop + `${s}:`, 'gm')
@@ -17,11 +19,41 @@ function parseScriptFile(file: string, type: string, prop: string) {
     // prop: () => {}
     // prop: function() {}
     // prop: function xxx() {}
-    // prop() {}
-    reg = new RegExp(`^${s}` + prop + `(${s}:${b}${s}=>|${s}:${s}function|${s}${b}${s}\\{)`, 'gm')
+    // prop: (e:{}) => {}
+    // reg = new RegExp(`^${s}` + prop + `(${s}:${b}${s}=>|${s}:${s}function|${s}${b}${s}\\{)`, 'gm')
+
+    /**
+     * 函数参数表正则
+     * 允许参数跨行
+     * - 无参数`()`
+     * - 有参数`( e )`
+     * - 参数跨行
+     * ```ts
+     *  (
+     *    e: event
+     *  )
+     * ```
+     */
+    const param = `\\([\\s\\S]*?\\)`
+    const async = `(async\\s+)?`
+    /**
+     * 方法定义正则
+     * - 普通方法`prop(...){`
+     * - 异步方法`async prop(...){`
+     */
+    const methodReg = `${async}${prop}${s}${param}${s}\\{`
+    /**
+     * 属性式函数定义 正则
+     * - 箭头函数`prop: (...) =>`
+     * - 异步箭头函数`prop: async (...) =>`
+     * - 普通函数声明`prop: function...`
+     * - 异步函数声明`prop: async function...`
+     */
+    const propFuncReg = `${prop}${s}:${async}(${param}${s}=>|function\\W)`
+    reg = new RegExp(`^${s}(${methodReg}|${propFuncReg})`, 'gm')
   }
 
-  match(content, reg as any).forEach(mat => {
+  match(content, reg!).forEach(mat => {
     let pos = getPositionFromIndex(content, mat.index + mat[0].indexOf(prop))
     let endPos = new Position(pos.line, pos.character + prop.length)
     locs.push(new Location(Uri.file(file), new Range(pos, endPos)))
