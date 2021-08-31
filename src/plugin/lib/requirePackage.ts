9bd591b90ad1891d0ec4c2aa0af0d702ab202f0c
iChenLei <chenleicoder@foxmail.com>
2021-08-31 17:34:00 +0800

    fix: can't find prettier module bugfix

--- src/plugin/lib/requirePackage.ts
+++ src/plugin/lib/requirePackage.ts
@@ -3,6 +3,9 @@ import * as resolve from 'resolve'
 import * as readPkgUp from 'read-pkg-up'
 import { existsSync } from 'fs'
 
+declare const __webpack_require__: typeof require;
+declare const __non_webpack_require__: typeof require;
+
 function findPkg(fspath: string, pkgName: string): string | undefined {
   const res = readPkgUp.sync({ cwd: fspath, normalize: false })
   const { root } = path.parse(fspath)
@@ -31,14 +34,19 @@ function findPkg(fspath: string, pkgName: string): string | undefined {
  */
 export function requireLocalPkg<T>(fspath: string, pkgName: string): T {
   let modulePath
+  // Source: https://github.com/microsoft/vscode-eslint/blob/master/server/src/eslintServer.ts
+  const r =
+    typeof __webpack_require__ === "function"
+      ? __non_webpack_require__
+      : require;
   try {
     modulePath = findPkg(fspath, pkgName)
     if (modulePath !== void 0) {
-      return require(modulePath)
+      return r(modulePath)
     }
   } catch (e) {
     console.warn(`Failed to load ${pkgName} from ${modulePath}. Using bundled.`)
   }
 
-  return require(pkgName)
+  return r(pkgName)
 }
